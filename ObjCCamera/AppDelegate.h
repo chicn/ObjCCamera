b24702c9394a11fe14981bdaa1c1cbec06407ae2
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2018-02-16 19:29:04 +0900

    Intial Commit

---
+++ ObjCCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCCamera
+//
+//  Created by Chihiro on 2018/02/16.
+//  Copyright © 2018 Chihiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCCamera
+//
+//  Created by Chihiro on 2018/02/16.
+//  Copyright © 2018 Chihiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
