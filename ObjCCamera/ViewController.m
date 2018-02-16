91fdd20a94f338971885c34c5776f363b8c8924e
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2018-02-16 19:49:04 +0900

    Add a delgate method

--- ObjCCamera/ViewController.m
+++ ObjCCamera/ViewController.m
@@ -60,4 +60,51 @@
     [self.session startRunning];
 }
 
+//delegate method which is called every frame
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    // render image
+    self.imageView.image = [self imageFromSampleBufferRef:sampleBuffer];
+}
+
+// Convert CMSampleBufferRef to UIImage
+- (UIImage *)imageFromSampleBufferRef:(CMSampleBufferRef)sampleBuffer
+{
+    // get an image buf
+    CVImageBufferRef    buffer;
+    buffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+
+    // Lock image buf
+    CVPixelBufferLockBaseAddress(buffer, 0);
+    // Get info from image buf
+    uint8_t*    base;
+    size_t      width, height, bytesPerRow;
+    base = CVPixelBufferGetBaseAddress(buffer);
+    width = CVPixelBufferGetWidth(buffer);
+    height = CVPixelBufferGetHeight(buffer);
+    bytesPerRow = CVPixelBufferGetBytesPerRow(buffer);
+
+    //  Create a bitmap context
+    CGColorSpaceRef colorSpace;
+    CGContextRef    cgContext;
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    cgContext = CGBitmapContextCreate(
+                                      base, width, height, 8, bytesPerRow, colorSpace,
+                                      kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+    CGColorSpaceRelease(colorSpace);
+
+    // Create image
+    CGImageRef  cgImage;
+    UIImage*    image;
+    cgImage = CGBitmapContextCreateImage(cgContext);
+    image = [UIImage imageWithCGImage:cgImage scale:1.0f
+                          orientation:UIImageOrientationUp];
+    CGImageRelease(cgImage);
+    CGContextRelease(cgContext);
+
+    // Unlock image buf
+    CVPixelBufferUnlockBaseAddress(buffer, 0);
+    return image;
+}
+
 @end
