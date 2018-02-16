e2ff86ffd0cec19c9e2122ed22c04d15201a9e46
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2018-02-16 19:41:44 +0900

    Add required lines to use caemra

--- ObjCCamera/ViewController.m
+++ ObjCCamera/ViewController.m
@@ -14,9 +14,50 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Get a device
+    AVCaptureDevice* device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    // Create input
+    AVCaptureDeviceInput* deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:NULL];
+
+    // Create video output
+    NSDictionary* settings = @{(id)kCVPixelBufferPixelFormatTypeKey:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA]};
+    AVCaptureVideoDataOutput* dataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    dataOutput.videoSettings = settings;
+    [dataOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
+
+    // Set up a session
+    self.session = [[AVCaptureSession alloc] init];
+    [self.session addInput:deviceInput];
+    [self.session addOutput:dataOutput];
+    self.session.sessionPreset = AVCaptureSessionPresetHigh;
+
+    AVCaptureConnection *videoConnection = NULL;
+
+    // Set up a camera settings
+    [self.session beginConfiguration];
+
+    for ( AVCaptureConnection *connection in [dataOutput connections] )
+    {
+        for ( AVCaptureInputPort *port in [connection inputPorts] )
+        {
+            if ( [[port mediaType] isEqual:AVMediaTypeVideo] )
+            {
+                videoConnection = connection;
+
+            }
+        }
+    }
+    if([videoConnection isVideoOrientationSupported]) // **Here it is, its always false**
+    {
+        [videoConnection setVideoOrientation:AVCaptureVideoOrientationPortrait];
+    }
+
+    [self.session commitConfiguration];
+    // Start the session
+    [self.session startRunning];
 }
 
 
